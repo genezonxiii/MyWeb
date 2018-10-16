13d9b647aef7fb11c98e87e5ce71e0108100c087
Melvin Chu <genezonxiii@msn.com>
2018-10-17 00:15:44 +0800

    first commit

---
+++ src/tw/com/myweb/IndexServlet.java
@@ -0,0 +1,36 @@
+package tw.com.myweb;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/IndexServlet")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public IndexServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("message", "Hello World");
+        request.setAttribute("model", model);
+        // Forward to JSP file to display message
+        RequestDispatcher dispatcher = request
+                .getRequestDispatcher("/WEB-INF/pages/index.jsp");
+        dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
